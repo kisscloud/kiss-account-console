a2dbf429096490e5cedd2493f6b438cdc3c336a4
koyeo <im@koyeo.io>
2018-10-11 13:31:56 +0800

    init

---
+++ src/main/java/com/kiss/console/KissConsoleApplication.java
@@ -0,0 +1,12 @@
+package com.kiss.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KissConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KissConsoleApplication.class, args);
+    }
+}
---
+++ src/main/java/com/kiss/console/controller/PageController.java
@@ -0,0 +1,13 @@
+package com.kiss.console.controller;
+
+
+public class PageController {
+
+    public void GetPagePermissionUsersParams(){
+
+//        return {
+//            "groups": List<Group>,
+//            "users": List<User>,
+//        }
+    }
+}
