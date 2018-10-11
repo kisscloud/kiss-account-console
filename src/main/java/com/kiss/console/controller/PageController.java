14954eb4e37a5248399a97c3cdd029ff026cb5d9
koyeo <im@koyeo.io>
2018-10-11 16:20:13 +0800

    demo

--- src/main/java/com/kiss/console/controller/PageController.java
+++ src/main/java/com/kiss/console/controller/PageController.java
@@ -6,6 +6,11 @@ public class PageController {
 
     /**
      * 获取用户管理页面参数
+     *
+     * 1. 查询所有的部门
+     * 2. 查询前50个用户
+     * 3. 查询用户列表，要支持分页
+     *
      */
     public void GetPagePermissionUsersParams() {
 
@@ -17,6 +22,9 @@ public class PageController {
 
     /**
      * 获取权限管理页面参数
+     *
+     * 1. 获取所有权限模块，按seq排序
+     * 2. 获取所有的权限
      */
     public void GetPagePermissionPermissionsParams() {
 
