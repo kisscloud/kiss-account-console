ffb105e0d4f0e6dd9bcc1d17eefab640c6f44bd7
qianrongli <qianrongli@uchain.one>
2018-10-11 16:30:14 +0800

    add kiss-common

--- src/main/java/com/kiss/console/controller/PageController.java
+++ src/main/java/com/kiss/console/controller/PageController.java
@@ -1,8 +1,12 @@
 package com.kiss.console.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PageController {
-
 
     /**
      * 获取用户管理页面参数
@@ -12,6 +16,7 @@ public class PageController {
      * 3. 查询用户列表，要支持分页   ?page=1&size=100
      *
      */
+    @GetMapping("/page/permission/users")
     public void GetPagePermissionUsersParams() {
 
 //        return {
@@ -26,6 +31,7 @@ public class PageController {
      * 1. 获取所有权限模块，按seq排序
      * 2. 获取所有的权限
      */
+    @GetMapping("/page/permission/permissions")
     public void GetPagePermissionPermissionsParams() {
 
 //        return {
