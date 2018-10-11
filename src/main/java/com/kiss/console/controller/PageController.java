0b99b10558ce78f696119f28bb7862669dd9f3c6
koyeo <im@koyeo.io>
2018-10-11 16:14:18 +0800

    demo

--- src/main/java/com/kiss/console/controller/PageController.java
+++ src/main/java/com/kiss/console/controller/PageController.java
@@ -3,7 +3,22 @@ package com.kiss.console.controller;
 
 public class PageController {
 
-    public void GetPagePermissionUsersParams(){
+
+    /**
+     * 获取用户管理页面参数
+     */
+    public void GetPagePermissionUsersParams() {
+
+//        return {
+//            "groups": List<Group>,
+//            "users": List<User>,
+//        }
+    }
+
+    /**
+     * 获取权限管理页面参数
+     */
+    public void GetPagePermissionPermissionsParams() {
 
 //        return {
 //            "groups": List<Group>,
