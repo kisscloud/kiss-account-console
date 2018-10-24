c117bdd2d2c6197a939df1491156b6220fe899b9
qianrongli <qianrongli@uchain.one>
2018-10-24 09:55:36 +0800

    add config

--- src/main/java/com/kiss/console/feign/account/PermissionServiceFeign.java
+++ src/main/java/com/kiss/console/feign/account/PermissionServiceFeign.java
@@ -2,10 +2,11 @@ package com.kiss.console.feign.account;
 
 import com.kiss.account.client.AccountClient;
 import com.kiss.account.client.PermissionClient;
+import com.kiss.console.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 
 @Component
-@FeignClient("kiss-account")
+@FeignClient(value = "kiss-account",configuration = FeignConfig.class)
 public interface PermissionServiceFeign extends PermissionClient {
 }
--- src/main/java/com/kiss/console/feign/account/RoleServiceFeign.java
+++ src/main/java/com/kiss/console/feign/account/RoleServiceFeign.java
@@ -2,10 +2,11 @@ package com.kiss.console.feign.account;
 
 import com.kiss.account.client.AccountClient;
 import com.kiss.account.client.RoleClient;
+import com.kiss.console.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 
 @Component
-@FeignClient("kiss-account")
+@FeignClient(value = "kiss-account",configuration = FeignConfig.class)
 public interface RoleServiceFeign extends RoleClient {
 }
